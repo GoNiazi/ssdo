f248f376271f522a6df4fb9c09d332ead10e153e
GoNiazi <komailniazi@gmail.com>
2022-11-04 16:52:28 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import "../styles/theme.css";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"
+      />
+      <Script src="/js/sticky.js" id="sticky" type="text/javascript" />
+      <Script src="/js/flickity.js" id="flickity" type="text/javascript" />
+      <Script src="/js/lazyload.js" id="lazyload" type="text/javascript" />
+      <Script
+        src="/js/smoothscroll.js"
+        id="smoothscroll"
+        type="text/javascript"
+      />
+      <Script src="/js/theme.js" id="theme" type="text/javascript" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
